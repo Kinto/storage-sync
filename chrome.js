87d11e0cf6af3eae4370f553a3bfb12645caf22c
Tarek Ziade <tarek@ziade.org>
2015-12-15 11:50:57 +0100

    first working version

--- chrome.js
+++ chrome.js
@@ -16,16 +16,57 @@ class StorageArea {
 
     this.db = new Kinto({
       remote: endpoint,
-      headers: {Authorization: "Basic dXNlcjpwYXNz"}
+      headers: {Authorization: "Basic dXNlcjpwYXNz"},
     });
 
-    this.items = this.db.collection("items");
+    function schema() {
+      let _next = 0;
+      return {
+        generate() {
+          return _next++;
+        },
+        validate(id) {
+          // validating everything for now
+          return true;
+        }
+      };
+    };
 
+    this.items = this.db.collection("items", {idSchema: schema()});
+
+    // XXX just for the tests
+    this.items.clear();
   }
 
   get(keys, callback) {
-    // todo: implement the list of keys.
-    return this.items.get(keys);
+    keys = [].concat(keys);
+    var this_items = this.items;
+    var records = [];
+
+    function getRecord(key) {
+      console.log("Looking for " + key);
+
+      return this_items.get(key).then(function (res) {
+        var promise = new Promise(function(resolve, reject) {
+          if (res) {
+            records.push(res.data);
+            resolve(res.data); 
+          } else {
+            reject('boom');
+          }
+        });
+        return promise;
+      });
+
+    }
+
+    Promise.all(keys.map(key => getRecord(key))).then(
+      function() {
+        console.log(records);
+        if (callback) callback(records);
+      }
+    );
+
   }
 
   getBytesInUse(keys, callback) {
@@ -34,8 +75,18 @@ class StorageArea {
 
   set(items, callback) {
     items = [].concat(items);
+    var this_items = this.items;
 
-    Promise.all(items.map(this.items.create)).then(
+    function createItem(item) {
+      var record = Object.values(item)[0];
+      record['id'] = Object.keys(item)[0];
+      console.log(record);
+      return this_items.create(record, {useRecordId: true});
+    }
+
+    Promise.all(items.map(item => createItem(item).then(
+              res => { console.log(res.data)}
+            ))).then(
       function() {if (callback) callback();}
     );
   }
